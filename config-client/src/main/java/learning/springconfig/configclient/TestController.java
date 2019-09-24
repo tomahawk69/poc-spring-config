902d392cc358959c5533cc60a79e341fe6d7a1ce
Yur Ovchynnikov <hawk.toma@gmail.com>
2019-09-24 17:55:20 +0200

    ''

--- config-client/src/main/java/learning/springconfig/configclient/TestController.java
+++ config-client/src/main/java/learning/springconfig/configclient/TestController.java
@@ -2,15 +2,17 @@ package learning.springconfig.configclient;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/service")
 @RequiredArgsConstructor
 public class TestController {
 
     private final TestService testService;
 
-    @GetMapping("/test")
+    @GetMapping("test")
     private String test() {
         return testService.props();
     }
