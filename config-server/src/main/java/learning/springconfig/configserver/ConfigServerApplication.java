f4f1c13a2a398cfe3963223e559f4f31d061b526
Yur Ovchynnikov <hawk.toma@gmail.com>
2019-11-11 20:45:33 +0100

    Test1

--- config-server/src/main/java/learning/springconfig/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/learning/springconfig/configserver/ConfigServerApplication.java
@@ -7,9 +7,11 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
+	// todo 1
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
+
 
 }
